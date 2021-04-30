b242634a27569730b0671e1bf5a93ca2f18f153a
CaicoLeung <lkk1053190902@gmail.com>
2021-04-30 21:09:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# miniprogram-cli
+
+CaicoLeung/gulp-wechat-miniprogram项目的脚手架
+
+## 特性和使用说明
+
+  点击查看: [gulp-wechat-miniprogram](https://github.com/CaicoLeung/gulp-wechat-miniprogram)
+
+## 开始使用
+
+- install安装
+
+    ```bash
+    yarn global add miniprogram-cli
+    ```
+
+    ```bash
+    npm install -g miniprogram-cli
+    ```
+
+- 初始化项目
+
+    ```bash
+    mp-cli init
+    ```
+
+- 查看版本
+
+    ```bash
+    mp-cli --version
+    ```
+
+- 查看帮助
+
+    ```bash
+    mp-cli --help
+    ```
---
+++ index.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+
+const download = require('download-git-repo')
+const inquirer = require('inquirer')
+const { program } = require('commander');
+const chalk = require('chalk')
+const ora = require('ora')
+const path = require('path')
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+
+program.version('1.0.0');
+
+const spinner = ora('正在下载模板, 请稍候...')
+
+const initConfig = {
+  name: 'my-miniprogram-app',
+  language: 'typescript',
+  style: 'scss'
+}
+
+const setProjectTypeHandler = (name, language, style) => {
+  try {
+    const projectPath = path.join(process.cwd(), name)
+    if (language === 'javasrcipt') {
+      fs.renameSync(path.join(projectPath, '/template/temp.ts'), path.join(projectPath, '/template/temp.js'))
+      fs.renameSync(path.join(projectPath, '/component/temp.ts'), path.join(projectPath, '/component/temp.js'))
+    }
+    if (['wxss', 'sass', 'less'].includes(style)) {
+      fs.renameSync(path.join(projectPath, '/template/temp.scss'), path.join(projectPath, `/template/temp.${style}`))
+      fs.renameSync(path.join(projectPath, '/component/temp.scss'), path.join(projectPath, `/component/temp.${style}`))
+    }
+    fsExtra.copySync('template', path.join(projectPath, '_template'))
+    fsExtra.copySync('component', path.join(projectPath, '_component'))
+  } catch (err) {
+    console.log(chalk.red(err.message));
+  }
+}
+
+const selectTypeHandler = (opts) => {
+  spinner.start()
+  const cloneErrHandler = (error) => {
+    if (error) {
+      spinner.fail()
+      console.log(chalk.red('failed! 拉取模板失败', error))
+      return
+    }
+    setProjectTypeHandler(opts.name, opts.language, opts.style)
+    spinner.succeed()
+    console.log(chalk.green('success! 项目初始化成功！'))
+    console.log(
+      chalk.greenBright('开启项目: ') + '\n' +
+      chalk.greenBright('cd ' + initConfig.name) + '\n' +
+      chalk.greenBright('yarn') + '\n' +
+      chalk.greenBright('开始开发~~~!')
+    );
+  };
+
+  if (opts.language === 'javascript') {
+    download('direct:https://github.com/CaicoLeung/gulp-wechat-miniprogram.git#template/js', path.join(process.cwd(), opts.name), { clone: true }, cloneErrHandler)
+  } else {
+    download('direct:https://github.com/CaicoLeung/gulp-wechat-miniprogram.git#template/ts', path.join(process.cwd(), opts.name), { clone: true }, cloneErrHandler)
+  }
+}
+
+program.command('init')
+  .description('初始化项目')
+  .action(async (name, opts) => {
+    const config = await inquirer.prompt([
+      {
+        name: 'name',
+        message: '请输入项目名称',
+        default: initConfig.name
+      },
+      {
+        name: 'language',
+        type: 'list',
+        message: '选择你的项目开发语言',
+        choices: ['javascript', 'typescript'],
+        default: initConfig.language
+      },
+      {
+        name: 'style',
+        type: 'list',
+        message: '选择你的项目css预处理语言',
+        choices: ['wxss', 'sass', 'scss', 'less'],
+        default: initConfig.style
+      }
+    ])
+
+    selectTypeHandler(config)
+  })
+
+program.parse(process.argv);
